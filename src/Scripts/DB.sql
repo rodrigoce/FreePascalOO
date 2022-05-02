8cec3ff5af8eb2add0c7bb749263120410024d27
Rodrigo <rodrigo.ce@gmail.com>
2022-05-01 21:26:19 -0300

    .

--- src/Scripts/DB.sql
+++ src/Scripts/DB.sql
@@ -1,3 +1,19 @@
 --** CRIAR O BANCO **
 -- abrir o ISQL TOOL e
 -- create database 'd:\rodrigo\freepascaloo\db\Aplicacao.fdb';
+
+create table PRODUTO
+(
+  ID Integer not null,
+  DATA_CRIACAO Timestamp,
+  DATA_ATUALIZACAO Timestamp,
+  DATA_EXCLUSAO Timestamp,
+  ID_USER_CRIACAO Integer,
+  ID_USER_ATUALIZACAO Integer,
+  ID_USER_EXCLUSAO Integer,
+  NOME VarChar(60)
+);
+
+alter table  PRODUTO add constraint PRODUTO_PK primary key (ID);
+
+ create sequence SEQUENCE_PRODUTO;
